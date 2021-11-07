a9d6910e33e2d04c749c74a1d3aa278eebbb4e59
willianchu <willianchu@hotmail.com>
2021-11-07 23:25:42 +0000

    basic return the last position of a search data in array

---
+++ lastIndexOf.js
@@ -0,0 +1,9 @@
+function lastIndexOf(array, value){
+  return array.lastIndexOf(value);
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
