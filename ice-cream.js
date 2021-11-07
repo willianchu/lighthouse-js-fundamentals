147c13f74e4f22c22ec3e0b05c416ab214fff1e6
willianchu <willianchu@hotmail.com>
2021-11-07 20:32:07 +0000

    commiting separated exercise of cloning arrays

--- ice-cream.js
+++ ice-cream.js
@@ -1,11 +1,14 @@
 // Do not modify these first two lines
 const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
-iceCreamFlavours.push("root beer");
 
+var iceCreamFlavours2 = Array.from(iceCreamFlavours);
+iceCreamFlavours2.push("root beer");
 
-// console.log((iceCreamFlavours.map()).push("root beer"));
-var iceCreamFlavours2 = (iceCreamFlavours);
-;
-// iceCreamFlavours2.push("root beer");
 console.log(iceCreamFlavours2);
-console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours2[0]);
+
+console.log(iceCreamFlavours2[iceCreamFlavours2.length-1]);
+
+console.log(iceCreamFlavours2.length);
+
