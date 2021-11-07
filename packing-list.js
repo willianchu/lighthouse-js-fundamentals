94720d84f41c5215fab58bcfe7abfe33efc76db7
willianchu <willianchu@hotmail.com>
2021-11-07 20:37:02 +0000

    while version of the same program

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,8 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
