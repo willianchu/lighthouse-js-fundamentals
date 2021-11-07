ef4751d557680d9db4886f349b55f7c7c67de33a
willianchu <willianchu@hotmail.com>
2021-11-07 22:47:10 +0000

    for while array manipulations

---
+++ ingredients.js
@@ -0,0 +1,21 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+};
+
+
+// Write a for loop that prints out the contents of ingredients:
+for(i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+};  
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+for(i = ingredients.length - 1; i >= 0; i--) {
+  console.log(ingredients[i]);
+};
+
+
